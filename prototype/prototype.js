dd46a6b8d3c51f74e56bef5f431f063df486eebe
swagath shetty <swagath.shetty@gmail.com>
2023-03-20 08:31:56 +0530

    added protypes

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,49 +1,108 @@
-class Address {
-    constructor(streetAddress,city,country){
-        this.streetAddress=streetAddress
-        this.city=city
-        this.country=country
-    }
+class Address
+{
+  constructor(streetAddress, city, country) {
+    this.streetAddress = streetAddress;
+    this.city = city;
+    this.country = country;
+  }
 
-    deepCopy(){
-        return new Address(this.streetAddress,this.city,this.country)
-    }
-
-    toString(){
-        return `Address ${this.streetAddress} ${this.city} ${this.country}`
-    }
-}
-class Person {
-    constructor(name,address){
-        this.name=name
-        this.address=address
-    }
-
-    deepCopy(){
-        return new Person(
-            this.name,
-            this.address.deepCopy()
-        )
-    }
-
-    toString(){
-        return `Name ${this.name} lives at ${this.address}`
-    }
+  toString()
+  {
+    return `Address: ${this.streetAddress}, ` +
+      `${this.city}, ${this.country}`;
+  }
 }
 
+class Person
+{
+  constructor(name, address)
+  {
+    this.name = name;
+    this.address = address; //!
+  }
 
-let swagath= new Person("swagath",new Address("ads nerul ","mumbai","India"))
+  toString()
+  {
+    return `${this.name} lives at ${this.address}`;
+  }
 
+  greet()
+  {
+    console.log(
+      `Hi, my name is ${this.name}, ` +
+      `I live at ${this.address.toString()}`
+    );
+  }
+}
 
-//to copy
-//BAD approach
-// let rahul=swagath
+class Serializer
+{
+  constructor(types){
+    this.types = types;
+  }
 
+  markRecursive(object)
+  {
+    // anoint each object with a type index
+    let idx = this.types.findIndex(t => {
+      return t.name === object.constructor.name;
+    });
+    if (idx !== -1)
+    {
+      object['typeIndex'] = idx;
 
-let rahul=swagath.deepCopy()
-rahul.name='rahul'
-rahul.address.streetAddress='214 street mahalaxmi'
+      for (let key in object)
+      {
+        if (object.hasOwnProperty(key) && object[key] != null)
+          this.markRecursive(object[key]);
+      }
+    }
+  }
 
+  reconstructRecursive(object)
+  {
+    if (object.hasOwnProperty('typeIndex'))
+    {
+      let type = this.types[object.typeIndex];
+      let obj = new type();
+      for (let key in object)
+      {
+        if (object.hasOwnProperty(key) && object[key] != null) {
+          obj[key] = this.reconstructRecursive(object[key]);
+        }
+      }
+      delete obj.typeIndex;
+      return obj;
+    }
+    return object;
+  }
 
-console.log(swagath.toString())
-console.log(rahul.toString())
+  clone(object)
+  {
+    this.markRecursive(object);
+    let copy = JSON.parse(JSON.stringify(object));
+    return this.reconstructRecursive(copy);
+  }
+}
+
+let john = new Person('John',
+  new Address('123 London Road', 'London', 'UK'));
+
+let jane = JSON.parse(JSON.stringify(john));
+
+jane.name = 'Jane';
+jane.address.streetAddress = '321 Angel St';
+
+john.greet();
+// this won't work
+// jane.greet();
+
+// try a dedicated serializer
+let s = new Serializer([Person,Address]); // pain point
+jane = s.clone(john);
+
+jane.name = 'Jane';
+jane.address.streetAddress = '321 Angel St';
+
+console.log(john.toString());
+console.log(jane.toString());
